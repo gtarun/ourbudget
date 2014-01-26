418534c2c527c410fe966991b060f49a18eefefa
Tarun Gupta <tarun.gupta@venturepact.com>
2014-01-26 12:41:54 +0530

    Added Login and signup on homepage

---
+++ API/users.js
@@ -0,0 +1,6 @@
+var userAPI = require('../models/User.js');
+var moment = require('moment');
+
+exports.adduser = function(req, res) {
+	console.log("API method getReportDataByExhchange called at " + new Date());
+}
